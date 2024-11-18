41af203e1d3605ab2ffc3a3ad44e9fd47c554e5e
aflores852023 <aflores85@gmail.com>
2024-11-18 10:49:47 -0300

    clave secreta

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,12 +4,12 @@ import ResponseBuilder from "../utils/builders/responseBuilders.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-export const registerUserController = async (req, res) => {
+export const registerUserController = async (req, res) => { 
     try{
-        const {name, email, password} = req.body
-        const existentUser = await User.findOne({email: email})
-        console.log({existentUser})
-        if(existentUser){
+        const {name, email, password} = req.body //Recibimos los datos del body
+        const existentUser = await User.findOne({email: email}) //Buscamos si el email ya esta en uso
+        //console.log({existentUser}) 
+        if(existentUser){ //Si el email ya esta en uso
             const response = new ResponseBuilder()
             .setOk(false)
             .setStatus(400)
@@ -23,12 +23,12 @@ export const registerUserController = async (req, res) => {
             return res.status(400).json(response)
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const verificationToken = jwt.sign({email: email}, ENVIROMENT.JWT_SECRET, {
+        const hashedPassword = await bcrypt.hash(password, 15) //15 es la cantidad de rondas
+        const verificationToken = jwt.sign({email: email}, ENVIROMENT.JWT_SECRET, { //Se genera el token de verificacion con la firma y el email, la firma es la clave secreta guardada en el .env
             expiresIn: '1d'
         })
 
-        const newUser = new User({
+        const newUser = new User({ //Se crea el usuario
             name,
             email,
             password: hashedPassword,
@@ -37,7 +37,7 @@ export const registerUserController = async (req, res) => {
         })
         await newUser.save()
 
-        const response = new ResponseBuilder()
+        const response = new ResponseBuilder() //Se envia la respuesta
         .setOk(true)
         .setStatus(200)
         .setMessage('Created')
@@ -45,7 +45,7 @@ export const registerUserController = async (req, res) => {
         .build()
         return res.status(201).json(response)
     }
-    catch(error){
+    catch(error){ //Si hay un error
         console.error('Error al registrar usuario:', error)
         const response = new ResponseBuilder()
         .setOk(false)
@@ -61,4 +61,3 @@ export const registerUserController = async (req, res) => {
     }
 
 }
-
