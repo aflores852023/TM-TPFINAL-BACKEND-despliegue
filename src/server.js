8ee044ed9ffe1241d4ec0d207ee72025f1fe0a5e
aflores852023 <aflores85@gmail.com>
2024-11-18 09:11:54 -0300

    conexion a mongoose

--- src/server.js
+++ src/server.js
@@ -1,38 +1,20 @@
-//servidor en express
-import express from 'express';
-import  ResponseBuilder from './utils/builders/responseBuilders.js';
+import ENVIROMENT from "./config/enviroment.config.js";
+import express from "express";
+import statusRouter from "./router/status.router.js";
+import mongoose from "./db/config.js"
+import authRouter from "./router/auth.router.js";
+
+
+
 
 const app = express();
-const PORT = 3000;
+const PORT = ENVIROMENT.PORT || 3000
 
-app.get('/ping', (req, res) => {
-    
-    try {
-        const response = new ResponseBuilder()
-            .setOk(true)
-            .setMessage('Success')
-            .setStatus(200)
-            .setPayload({
-                message: 'Pong'
-            })
-            .build()
-            res.status(200).json(response)  
-    } catch (error) {
-        response
-            .setOk(false)
-            .setMessage('Interna server Error')
-            .setStatus(500)
-            .setPayload({
-                message: error.message
-            })
-            .build()
-            res.status(500).json(response)
-    }
-    
-});
-        
-        
+app.use(express.json())
+
+app.use('/api/status', statusRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto https://localhost:${PORT}`);
-});
+    console.log(`El servidor se esta escuchando en http://localhost:${PORT}`)
+})
