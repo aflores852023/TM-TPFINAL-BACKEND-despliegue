e25c5ed8dd792cbfb351161c0c53231d70668b61
aflores852023 <aflores85@gmail.com>
2024-11-23 18:15:27 -0300

    serjer.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
 app.use(express.json({ limit: '5mb' }));
 app.use('/api/status', statusRouter);
 app.use('/api/auth', authRouter);
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send('Backend operativo');
 });
 
