e3d22d8cb29693a25dee83151defbd5924a89e9a
aflores852023 <aflores85@gmail.com>
2024-12-11 09:56:29 -0300

    ssl mongose

--- src/db/config.js
+++ src/db/config.js
@@ -1,17 +1,23 @@
 import mongoose from "mongoose";
 import ENVIROMENT from "../config/enviroment.config.js";
 
-
-mongoose.connect(ENVIROMENT.DB_URL)
-.then(
-    () => {
-        console.log('Conexion exitosa con la DB!')
+const connectDB = async () => {
+    try {
+        // Configuración de conexión con parámetros adicionales
+        await mongoose.connect(ENVIROMENT.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true, // Asegura que SSL esté habilitado
+            tlsAllowInvalidCertificates: false, // Rechaza certificados no válidos
+        });
+        console.log("Conexión exitosa a MongoDB Atlas");
+    } catch (error) {
+        console.error("Error de conexión a MongoDB:", error.message);
+        process.exit(1); // Salida del proceso en caso de error crítico
     }
-)
-.catch(
-    (error) => {
-        console.error('Error de conexion')
-    }
-)
+};
 
-export default mongoose
+// Llamada a la función de conexión
+connectDB();
+
+export default mongoose;
